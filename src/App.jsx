1fab70074c8e6faf84d53a235d63a9cfcd3a1b77
unknown <gabhishekh1@gmail.com>
2026-01-08 14:35:22 +0530

    new message

--- src/App.jsx
+++ src/App.jsx
@@ -10,45 +10,7 @@ import img5 from './assets/techo.jpg'
 import img6 from './assets/laptop.png'
 import img7 from './assets/technology.jpg'
 import Navbar from './Navbar.jsx'
-
-
-
-function Header(){
-  return(
-       <div style={{
-          position: "fixed",
-          top: 0,
-          left: 305,
-          width: "67%",
-          height: "10vh",
-          backgroundColor: "lightgray", 
-          display: "flex",
-          alignItems: "center",
-          paddingLeft: "15px",        
-          borderRadius: "15px",      
-          color: "white", 
-          border:"3px solid purple",            
-          fontWeight: "600",
-          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)" 
-        }}>
-        {/* Logo on the left */}
-       <img src={img6} alt="logo"
-          style={{
-              height: "50px",
-              width: "50px",
-              borderRadius:"50%"
-          }}/>
-
-          {/* Heading text */}
-        <h1 style={{
-            color: "purple",
-            margin: 0,
-            fontSize: "24px"}}>Gupta Tech Store</h1>
-          </div>
-  );
-}
-
-
+import Header from './Header.jsx'
 
 function Form(){
   const [name,setName]=useState('');
